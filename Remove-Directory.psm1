2e7293682b860cceb90b5ef0074f5e2a076c3836
Enneman <you@example.com>
2018-11-11 20:08:54 +0100

    Change Clean-Directory references to Remove-Directory

--- Remove-Directory.psm1
+++ Remove-Directory.psm1
@@ -4,7 +4,7 @@
 Remove files older than a defined amount of time from a given directory.
 
 .DESCRIPTION
-The Clean-Directory function removes files older than a defined amount of time (months, days, hours or minutes) from a given directory
+The Remove-Directory function removes files older than a defined amount of time (months, days, hours or minutes) from a given directory
 This function is written for servers that install apps on the D: Drive, so actions on the C: Drive are forbidden.
 
 .PARAMETER directory
@@ -18,7 +18,7 @@ set the amount of time units
 
 .EXAMPLE
 Remove files older than 3 days from D:\temp
-Clean-Directory -directory "D:\temp" -interval days -count 3
+Remove-Directory -directory "D:\temp" -interval days -count 3
 
 .NOTES
 
@@ -28,7 +28,7 @@ Clean-Directory -directory "D:\temp" -interval days -count 3
 function Remove-Directory {
   <#
     .SYNOPSIS
-    Describe purpose of "Clean-Directory" in 1-2 sentences.
+    Describe purpose of "Remove-Directory" in 1-2 sentences.
 
     .DESCRIPTION
     Add a more complete description of what the function does.
@@ -43,7 +43,7 @@ function Remove-Directory {
     Describe parameter -count.
 
     .EXAMPLE
-    CleanDirectory -directory Value -interval Value -count Value
+    Remove-Directory -directory Value -interval Value -count Value
     Describe what this call does
 
 
@@ -52,7 +52,7 @@ function Remove-Directory {
 
     .LINK
     URLs to related sites
-    The first link is opened by Get-Help -Online CleanDirectory
+    The first link is opened by Get-Help -Online Remove-Directory 
 
     .INPUTS
     List of input types that are accepted by this function.
@@ -78,11 +78,6 @@ function Remove-Directory {
         PROCESS {
             
             $null = ($directory -split ':',2)[0]
-            # check if D: disk
-            #if ($disk -eq 'C') {
-            #    Write-Verbose -Message 'C: Drive not allowed'
-            #    break
-            #}
 
             $limit=''
         
